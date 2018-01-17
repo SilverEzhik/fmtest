74b80bc27d21d1e293371b3c55bb1ec5506e6528
Ezhik <i@ezhik.me>
2018-01-16 23:52:34 -0500

    Implement folder update notifications via sockets, clean up path handling in millertoy

--- millertoy.go
+++ millertoy.go
@@ -11,6 +11,76 @@ import (
 	"net/http"
 )
 
+var folderWatchers map[string]chan struct{} = make(map[string]chan struct{})
+
+//All messages will be broadcast to everyone - so this is not suitable for multiple users.
+func broadcast(message string) {
+	for _, socket := range server.Sockets() {
+		socket.Write(message)
+	}
+}
+
+func watchFolder(path string) {
+	fmt.Println(path)
+
+	var f fm.Folder
+	f, err := fs.GetFolder(path)
+	if err != nil {
+		fmt.Println("error:", err)
+		return
+	}
+
+	update := f.Watch()
+
+	for {
+		select {
+		case <-folderWatchers[path]:
+			f.Close()
+			delete(folderWatchers, path)
+			return
+		case <-update:
+
+			if f.Path() == "" && f.Contents() == nil {
+				fmt.Println("folder object gone")
+				message := JSONUpdate{Path: path, Deleted: true}
+				jsonMsg, _ := json.Marshal(message)
+				broadcast(string(jsonMsg))
+				delete(folderWatchers, path)
+				break
+			} else {
+				message := JSONUpdate{Path: path, Deleted: false, Contents: fm.GetSortedFileList(f)}
+				if path != f.Path() {
+					message.NewPath = f.Path()
+					folderWatchers[f.Path()] = folderWatchers[path]
+					delete(folderWatchers, path)
+					path = f.Path()
+				}
+				jsonMsg, _ := json.Marshal(message)
+				broadcast(string(jsonMsg))
+			}
+		}
+	}
+}
+func closeDir(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("close dir")
+	vars := mux.Vars(r)
+	pathId := vars["path"]
+	var path string
+
+	if len(pathId) == 0 {
+		path = "/"
+	} else if string(pathId[0]) == "~" {
+		path = pathId
+	} else {
+		path = "/" + pathId
+	}
+
+	fmt.Println("path:\"" + path + "\"")
+
+	close(folderWatchers[path])
+	fmt.Fprintln(w, "ok")
+}
+
 func getDir(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("get dir")
 	vars := mux.Vars(r)
@@ -26,6 +96,8 @@ func getDir(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Println("path:\"" + path + "\"")
+	folderWatchers[path] = make(chan struct{})
+	go watchFolder(path)
 
 	a := JSONFolder{Path: path, Contents: make([]string, 0)}
 
@@ -44,13 +116,20 @@ func getDir(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(path, len(a.Contents))
 }
 
+type JSONUpdate struct {
+	Path     string   `json:"path"`
+	NewPath  string   `json:"newPath"`
+	Deleted  bool     `json:"deleted"`
+	Contents []string `json:"contents"`
+}
+
 type JSONFolder struct {
 	Path     string   `json:"path"`
 	Contents []string `json:"contents"`
 	Error    error    `json:"error"`
 }
 
-func watchFolder(path string) {
+func refreshFolder(path string) {
 	fmt.Println(path)
 
 	var f fm.Folder
@@ -70,7 +149,6 @@ Loop:
 				fmt.Println("folder object gone")
 				break Loop
 			}
-
 			for _, socket := range server.Sockets() {
 				socket.Write("Folder update")
 			}
@@ -94,6 +172,9 @@ func main() {
 
 	//read files
 	router.HandleFunc(`/api/open/{path:.*}`, getDir).Methods("GET")
+
+	//close folders
+	router.HandleFunc(`/api/close/{path:.*}`, closeDir).Methods("GET")
 
 	//notify api
 	// Create a new glue server.
@@ -121,7 +202,7 @@ func main() {
 	staticFileHandler := http.StripPrefix("/", http.FileServer(staticFileDirectory))
 	router.PathPrefix("/").Handler(staticFileHandler).Methods("GET")
 	//check static updates
-	go watchFolder("./millertoy_html")
+	go refreshFolder("./millertoy_html")
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
--- millertoy_html/script.js
+++ millertoy_html/script.js
@@ -2,7 +2,7 @@ function Folder(path) {
     this.path = path;
     var n = path.lastIndexOf('/');
     name = path;
-    if (n > 0) {
+    if (n != -1 && path != "/") {
         name = name.substring(n + 1);
     }
     this.name = name;
@@ -23,29 +23,15 @@ function Folder(path) {
             } else {
                 arr = [];
                 for (var i = 0; i < f.contents.length; i++) {
-                    arr[i] = new Folder(this.path + "/" + f.contents[i])               
+                    p = this.path + "/" + f.contents[i];              
+                    p = p.replace("//", "/");
+                    arr[i] = new Folder(p);
                 }
                 return arr;
             }
         }
     }
 }
-
-/*
-//fake root to test
-var root = new Folder("root");
-
-for (var i = 0; i < 20; i++) {
-    root.contents[i] = new Folder("child " + i);
-    for (var j = 0; j < 10; j++) {
-        root.contents[i].contents[j] = new Folder("subchild " + j);
-        for (var k = 0; k < 5; k++) {
-            root.contents[i].contents[j].contents[k] = new Folder("subsubchild " + k);
-            root.contents[i].contents[j].contents[k].contents[0] = new Folder("subsubsubchild 1");
-        }
-    }
-}
-*/
 
 function Column(folder) {
     this.folder = folder
