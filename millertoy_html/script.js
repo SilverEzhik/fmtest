d66442fa8bba9418867e74fae55442dd39547934
Ezhik <i@ezhik.me>
2018-01-17 23:57:24 -0500

    Refactor millertoy js a little to not nest folder objects

--- millertoy_html/script.js
+++ millertoy_html/script.js
@@ -23,9 +23,7 @@ function Folder(path) {
             } else {
                 arr = [];
                 for (var i = 0; i < f.contents.length; i++) {
-                    p = this.path + "/" + f.contents[i];              
-                    p = p.replace("//", "/");
-                    arr[i] = new Folder(p);
+                    arr[i] = f.contents[i];
                 }
                 return arr;
             }
@@ -33,16 +31,26 @@ function Folder(path) {
     }
 }
 
+function getClickedFolder(column, name) {
+    return getPath(column.folder.path, name);
+}
+
+function getPath(parentPath, childName) {
+    p = parentPath + "/" + childName;              
+    p = p.replace("//", "/");
+    return p;
+}
+
 function Column(folder) {
     this.folder = folder
-    this.selected = null
+    this.active = null
     this.HTMLElement = null
 }
 
 var columns = [];
 
 
-function makeHTMLColumn(column) {
+function makeHTMLColumn(column, array) {
     var col = document.createElement("div");
     column.HTMLElement = col;
     var title = document.createElement("b");
@@ -59,9 +67,9 @@ function makeHTMLColumn(column) {
         var list = col.appendChild(document.createElement("ul"));
         for (var i = 0; i < files.length; i++) {
             var folder = files[i];
-            if (files[i].name[0] != ".") {
+            if (files[i][0] != ".") {
                 var item = document.createElement("li");
-                item.appendChild(document.createTextNode(folder.name));
+                item.appendChild(document.createTextNode(folder));
                 item.onclick = (function() {
                     var folder = files[i]; 
                     return function() {clickColumn(column, folder);}
@@ -76,22 +84,27 @@ function appendColumn(colNode) {
     document.getElementById("miller-container").appendChild(makeHTMLColumn(colNode));
 }
 
-function clickColumn(column, folder) {
+function markActive(column) {
     var list = column.HTMLElement.childNodes[1].childNodes;
     for (var i = 0; i < list.length; i++) {
-        if (list[i].innerText != folder.name) {
+        if (list[i].innerText != column.active) {
             list[i].classList.remove("item-active");
         } else {
             list[i].classList.add("item-active");
         }
     }
+}
+
+function clickColumn(column, name) {
+    column.active = name;
+    markActive(column)
     index = columns.indexOf(column);
     for (var i = index + 1; i < columns.length; i++) {
         document.getElementById("miller-container").removeChild(columns[i].HTMLElement);
     }
         
     columns.length = index + 1;
-    columns[index + 1] = new Column(folder);
+    columns[index + 1] = new Column(getFolder(getClickedFolder(column, name))); //so this is the javascript power...
     appendColumn(columns[index + 1]);
 }
 function getFolder(path) {
@@ -112,3 +125,98 @@ function getFolder(path) {
 
 columns[0] = new Column(getFolder("/"));
 appendColumn(columns[0]);
+
+function refreshFolder(data) {
+    f = JSON.parse(data);
+    if (f.error != null) {
+        return; //emptry
+    } else {
+    }
+}
+
+var host = "http://localhost:8080";
+
+var opts = {
+    // The base URL is appended to the host string. This value has to match with the server value.
+    baseURL: "/channel/",
+
+    // Force a socket type.
+    // Values: false, "WebSocket", "AjaxSocket"
+    forceSocketType: false,
+
+    // Kill the connect attempt after the timeout.
+    connectTimeout:  10000,
+
+    // If the connection is idle, ping the server to check if the connection is stil alive.
+    pingInterval:           35000,
+    // Reconnect if the server did not response with a pong within the timeout.
+    pingReconnectTimeout:   5000,
+
+    // Whenever to automatically reconnect if the connection was lost.
+    reconnect:          true,
+    reconnectDelay:     1000,
+    reconnectDelayMax:  5000,
+    // To disable set to 0 (endless).
+    reconnectAttempts:  10,
+
+    // Reset the send buffer after the timeout.
+    resetSendBufferTimeout: 10000
+};
+
+// Create and connect to the server.
+// Optional pass a host string and options.
+var socket = glue(host, opts);
+
+socket.onMessage(function(data) {
+    console.log("onMessage: " + data);
+
+    if (data == "Folder update") {
+        // https://zeit.co/blog/async-and-await
+        function sleep (time) {
+          return new Promise((resolve) => setTimeout(resolve, time));
+        }
+
+        // Usage!
+        sleep(500).then(() => {
+            location.reload()
+            // Do something after the sleep!
+        })
+    }
+    refreshFolder(data)
+
+    // Echo the message back to the server.
+    //socket.send("echo: " + data);
+});
+
+
+socket.on("connected", function() {
+    console.log("connected");
+});
+
+socket.on("connecting", function() {
+    console.log("connecting");
+});
+
+socket.on("disconnected", function() {
+    console.log("disconnected");
+});
+
+socket.on("reconnecting", function() {
+    console.log("reconnecting");
+});
+
+socket.on("error", function(e, msg) {
+    console.log("error: " + msg);
+});
+
+socket.on("connect_timeout", function() {
+    console.log("connect_timeout");
+});
+
+socket.on("timeout", function() {
+    console.log("timeout");
+});
+
+socket.on("discard_send_buffer", function() {
+    console.log("some data could not be send and was discarded.");
+});
